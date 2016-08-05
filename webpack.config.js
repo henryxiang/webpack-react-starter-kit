f32f76af2e6bf72e30f13486dab6c80d5caccf4d
Henry Xiang <henryxiang@gmail.com>
2016-08-04 17:14:40 -0700

    changed webpack output directory to build/

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
-// var webpack = require('webpack');
 module.exports = {
   // Entry point for the bundle, path and filename to main module
   entry: './src/main.js',
+
   output: {
     // Output directory as an absolute path
-    path: './src',
+    path: './build',
     // Filename as relative path within output path
     filename: 'bundle.js',
     // Output path from the view of the JS/HTML
     // publicPath: '/${4:output_publicPath}/'
   },
+
   module: {
     // Loader required for JSX
     loaders: [{
@@ -18,9 +19,10 @@ module.exports = {
       loader: 'babel-loader'
     }]
   },
+
   devServer: {
     inline: true,
-    contentBase: './src',
+    contentBase: './build',
     port: 8000
   }
-};
+}
