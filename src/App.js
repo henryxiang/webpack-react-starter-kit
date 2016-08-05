094bd0752c6bbb541f2e945ab800c865077dc4bf
Henry Xiang <henryxiang@gmail.com>
2016-08-04 21:25:07 -0700

    enabled ESLint; created React Router demo app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Router, Route, Link, browserHistory} from 'react-router';
+// import _ from 'lodash';
+
+const Home = () => <div><Links />Home</div>
+const About = () => <div><Links />About</div>
+const Contact = () => <div><Links />Contact</div>
+
+const Links = () =>
+  <ul>
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/about">About</Link></li>
+    <li><Link to="/contact">Contact</Link></li>
+  </ul>
 
 class App extends Component {
   render() {
     return (
-      <div>React Demo</div>
+      <Router history={browserHistory}>
+        <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Router>
     );
   }
 }
